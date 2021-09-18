ad2835f164962f6c3c3c6afea5840914ea8b75b1
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 20:18:06 +0300

    Homework 4

--- src/test.java
+++ src/test.java
@@ -1,13 +1,33 @@
-import java.util.Scanner;
 
-public class test {
-    public static void main (String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int number=scanner.nextInt();
-        if ( number % 2 == 0 )
-            System.out.println("Четное");
-        else
-            System.out.println("Нечетное");
+import java.util.Scanner;
+    public class test {public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int day = scanner.nextInt();
+            switch (day) {
+                case (1):
+                    System.out.println("Понедельник");
+                    break;
+                case (2):
+                    System.out.println("Вторник");
+                    break;
+                case (3):
+                    System.out.println("Среда");
+                    break;
+                case (4):
+                    System.out.println("Четверг");
+                    break;
+                case (5):
+                    System.out.println("Пятница");
+                    break;
+                case (6):
+                    System.out.println("Суббота");
+                    break;
+                case (7):
+                    System.out.println("Воскресенье");
+                    break;
+                default:
+                    System.out.println("Некорректное число");
+                    break;
+            }
         }
     }
-
