b016641f09573b364a88d3c50c3f1f692352d84f
OrangeBLR <alex.shepturo@gmail.com>
2021-09-22 20:51:51 +0300

    Homework 5

--- src/test.java
+++ src/test.java
@@ -1,33 +1,18 @@
-
 import java.util.Scanner;
-    public class test {public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int day = scanner.nextInt();
-            switch (day) {
-                case (1):
-                    System.out.println("Понедельник");
-                    break;
-                case (2):
-                    System.out.println("Вторник");
-                    break;
-                case (3):
-                    System.out.println("Среда");
-                    break;
-                case (4):
-                    System.out.println("Четверг");
-                    break;
-                case (5):
-                    System.out.println("Пятница");
-                    break;
-                case (6):
-                    System.out.println("Суббота");
-                    break;
-                case (7):
-                    System.out.println("Воскресенье");
-                    break;
-                default:
-                    System.out.println("Некорректное число");
-                    break;
-            }
-        }
+
+public class test {public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Количество элементов массива: ");
+    int size = input.nextInt();
+    int array[] = new int[size];
+    System.out.println("Введите элементы массива:");
+    for (int i = 0; i < size; i++) {
+        array[i] = input.nextInt();
     }
+    System.out.print ("Ваш массив:");
+    for (int i = 0; i < size; i++) {
+        System.out.print (" " + array[i]);
+    }
+    System.out.println();
+}
+}
