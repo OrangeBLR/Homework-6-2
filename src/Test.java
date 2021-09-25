6b4f7bdc691164de2bf1e329d16c3eb24d655808
OrangeBLR <alex.shepturo@gmail.com>
2021-09-25 19:51:14 +0300

    Homework 6-1

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr1 = createArray();
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(" " + arr1[i]);
+        }
+    }
+
+    static int[] createArray() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Количество элементов массива: ");
+        int size = input.nextInt();
+        int array[] = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        return array;
+    }
+}
